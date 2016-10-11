eb1206ad11a3b4c6ed3e65f22cb12da421bfa88e
haiwei <123987293@qq.com>
2016-10-11 13:00:44 +0800

    初始提交

---
+++ testNode/app/index/components/Favlist.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+    <div v-for="n in 10">{{msg}}</div>
+	</div>
+</template>
+
+<script type="text/javascript">
+    export default {
+        data () {
+            return {
+                msg: 'Hello World!'
+            }
+        }
+    }
+</script>
+
+<style>
+    html{
+        background: red;
+    }
+</style>
