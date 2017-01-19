de904caa2ce336b9d7e0c3d0062735d5e9b48bf4
haiwei <123987293@qq.com>
2017-01-19 17:11:58 +0800

    232

--- houseVue/src/components/TopHeader.vue
+++ houseVue/src/components/TopHeader.vue
@@ -1,7 +1,7 @@
 <template>
 	<header class="bar bar-nav">
 		<a class="button button-link button-nav pull-left" v-link="{path: path+(pid||''), replace: true}">
-	    	<span class="icon icon-left"></span>
+	    	<span class="icon icon-back"></span>
 	    </a>
     	<div class="title" v-text="label"></div>
   </header>
