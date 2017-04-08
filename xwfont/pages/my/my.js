f925eca7c6a8a1cad731a8f5c564786316a2f986
haiwei <123987293@qq.com>
2017-04-08 16:45:13 +0800

    可以

--- xwfont/pages/my/my.js
+++ xwfont/pages/my/my.js
@@ -3,9 +3,9 @@ var app = getApp();
 Page({
   data:{
     gridList: [
-      {enName:'favorite', zhName:'收藏'},
-      {enName:'history', zhName:'浏览记录'},
-      {enName:'shake', zhName:'摇一摇'},
+      {enName:'favorite', zhName:'我的操作'},
+      // {enName:'history', zhName:'浏览记录'},
+      // {enName:'shake', zhName:'摇一摇'},
       {enName:'gallery', zhName:'相册'},
       {enName:'setting', zhName:'设置'}
     ],
